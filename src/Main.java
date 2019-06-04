6ff756d04b3e70bf0b3ac85979b1f8acbf475cea
porter_moody <porter_moody@student.wayland.k12.ma.us>
2019-06-04 12:52:55 -0400

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,93 @@
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * PORTER MOODY
+ */
+public class Main extends JPanel implements ActionListener, KeyListener, MouseListener{
+    private Timer timer;
+    private int pace;
+
+    public Main(int w, int h){
+        setSize(w, h);
+
+
+        //world.test();
+        pace = 100;
+        timer = new Timer(pace, this);
+        timer.start();
+
+        addKeyListener(this);
+        addMouseListener(this);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("HI PORTER");
+        JFrame frame = new JFrame("Tetris");
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        int width = 800;
+        int height = 800;
+        frame.setPreferredSize(new Dimension(width, height + 24));
+
+        JPanel panel = new Main(width, height);
+        panel.setFocusable(true);
+        panel.grabFocus();
+
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
