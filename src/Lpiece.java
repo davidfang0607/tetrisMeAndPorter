f6c4b827982770dbe31480c723f3f5330ae011f8
david_fang <david_fang@student.wayland.k12.ma.us>
2019-06-08 23:37:41 -0400

    changes piece classes

--- src/Lpiece.java
+++ src/Lpiece.java
@@ -9,25 +9,25 @@ public class Lpiece extends Block{
         super(x,y,Color.blue);
 
         //here's where we hardcode coordinates...
-        rotIndexL[0][0] = new Point(0, 0);
-        rotIndexL[0][1] = new Point(1, 0);
-        rotIndexL[0][2] = new Point(2, 0);
-        rotIndexL[0][3] = new Point(2, 1);
+        rotIndexL[0][0] = new Point(0, 2);
+        rotIndexL[0][1] = new Point(1, 2);
+        rotIndexL[0][2] = new Point(1, 1);
+        rotIndexL[0][3] = new Point(1, 0);
 
-        rotIndexL[1][0] = new Point(1, 0);
-        rotIndexL[1][1] = new Point(0, 0);
-        rotIndexL[1][2] = new Point(0, 1);
-        rotIndexL[1][3] = new Point(0, 2);
+        rotIndexL[1][0] = new Point(0, 1);
+        rotIndexL[1][1] = new Point(1, 1);
+        rotIndexL[1][2] = new Point(2, 1);
+        rotIndexL[1][3] = new Point(2, 2);
 
-        rotIndexL[2][0] = new Point(0, 0);
+        rotIndexL[2][0] = new Point(0, 2);
         rotIndexL[2][1] = new Point(0, 1);
-        rotIndexL[2][2] = new Point(1, 1);
-        rotIndexL[2][3] = new Point(2, 2);
+        rotIndexL[2][2] = new Point(0, 0);
+        rotIndexL[2][3] = new Point(1, 0);
 
         rotIndexL[3][0] = new Point(0, 0);
-        rotIndexL[3][1] = new Point(1, 0);
+        rotIndexL[3][1] = new Point(0, 1);
         rotIndexL[3][2] = new Point(1, 1);
-        rotIndexL[3][3] = new Point(1, 2);
+        rotIndexL[3][3] = new Point(2, 1);
     }
 
 }
--- src/RLpiece.java
+++ src/RLpiece.java
@@ -9,25 +9,25 @@ public class RLpiece extends Block{
         super(x,y,Color.orange);
 
         //here's where we hardcode coordinates...
-        rotIndexrL[0][0] = new Point(0, 1);
-        rotIndexrL[0][1] = new Point(1, 1);
-        rotIndexrL[0][2] = new Point(2, 1);
-        rotIndexrL[0][3] = new Point(2, 0);
+        rotIndexrL[0][0] = new Point(0, 0);
+        rotIndexrL[0][1] = new Point(1, 0);
+        rotIndexrL[0][2] = new Point(1, 1);
+        rotIndexrL[0][3] = new Point(1, 2);
 
-        rotIndexrL[1][0] = new Point(0, 0);
-        rotIndexrL[1][1] = new Point(1, 0);
+        rotIndexrL[1][0] = new Point(0, 2);
+        rotIndexrL[1][1] = new Point(0, 1);
         rotIndexrL[1][2] = new Point(1, 1);
-        rotIndexrL[1][3] = new Point(1, 2);
+        rotIndexrL[1][3] = new Point(2, 1);
 
-        rotIndexrL[2][0] = new Point(0, 1);
-        rotIndexrL[2][1] = new Point(0, 0);
-        rotIndexrL[2][2] = new Point(1, 0);
-        rotIndexrL[2][3] = new Point(2, 0);
+        rotIndexrL[2][0] = new Point(0, 0);
+        rotIndexrL[2][1] = new Point(0, 1);
+        rotIndexrL[2][2] = new Point(0, 2);
+        rotIndexrL[2][3] = new Point(1, 2);
 
-        rotIndexrL[3][0] = new Point(0, 0);
-        rotIndexrL[3][1] = new Point(0, 1);
-        rotIndexrL[3][2] = new Point(0, 2);
-        rotIndexrL[3][3] = new Point(1, 2);
+        rotIndexrL[3][0] = new Point(0, 1);
+        rotIndexrL[3][1] = new Point(1, 1);
+        rotIndexrL[3][2] = new Point(2, 1);
+        rotIndexrL[3][3] = new Point(2, 0);
     }
 
 }
