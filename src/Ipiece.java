7414d69ae7ec162b3736987751346494f241d833
david_fang <david_fang@student.wayland.k12.ma.us>
2019-06-10 13:48:25 -0400

    changes pieces' index orders to match the rotation method, T shape and I shape not fixed, third rotation always moves left 1 unit.

--- src/Ipiece.java
+++ src/Ipiece.java
@@ -11,25 +11,25 @@ public class Ipiece extends Block{
         super(y,x,new Color(72, 228, 233));
 
         //here's where we hardcode coordinates...
-        rotations[0][0] = new Point(0, 0);
-        rotations[0][1] = new Point(0, 1);
-        rotations[0][2] = new Point(0, 2);
-        rotations[0][3] = new Point(0, 3);
+        rotations[3][0] = new Point(0, 0);
+        rotations[3][1] = new Point(0, 1);
+        rotations[3][2] = new Point(0, 2);
+        rotations[3][3] = new Point(0, 3);
 
-        rotations[1][0] = new Point(0, 2);
-        rotations[1][1] = new Point(1, 2);
-        rotations[1][2] = new Point(2, 2);
-        rotations[1][3] = new Point(3, 2);
+        rotations[2][0] = new Point(0, 2);
+        rotations[2][1] = new Point(1, 2);
+        rotations[2][2] = new Point(2, 2);
+        rotations[2][3] = new Point(3, 2);
 
-        rotations[2][0] = new Point(0, 0);
-        rotations[2][1] = new Point(0, 1);
-        rotations[2][2] = new Point(0, 2);
-        rotations[2][3] = new Point(0, 3);
+        rotations[1][0] = new Point(0, 0);
+        rotations[1][1] = new Point(0, 1);
+        rotations[1][2] = new Point(0, 2);
+        rotations[1][3] = new Point(0, 3);
 
-        rotations[3][0] = new Point(0, 1);
-        rotations[3][1] = new Point(1, 1);
-        rotations[3][2] = new Point(2, 1);
-        rotations[3][3] = new Point(3, 1);
+        rotations[0][0] = new Point(0, 1);
+        rotations[0][1] = new Point(1, 1);
+        rotations[0][2] = new Point(2, 1);
+        rotations[0][3] = new Point(3, 1);
     }
 
 //    @Override
